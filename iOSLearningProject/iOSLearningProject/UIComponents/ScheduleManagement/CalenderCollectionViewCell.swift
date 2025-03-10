327b54941856f03414e572a4d2e9d1f37f376cab
Srushti Sarju Engineer <srushti.engineer@simformsolutions.com>
2025-03-10 13:18:25 +0530

    TE-T282: Schedule management UI

---
+++ iOSLearningProject/iOSLearningProject/UIComponents/ScheduleManagement/CalenderCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CalenderCollectionViewCell.swift
+//  UIOne
+//
+//  Created by Srushti Sarju Engineer on 04/03/25.
+//
+
+import UIKit
+
+class CalenderCollectionViewCell: UICollectionViewCell {
+
+//    @IBOutlet weak var lblDay: UILabel!
+//    @IBOutlet weak var lblDate: UILabel!
+//    @IBOutlet weak var viewDateDay: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
