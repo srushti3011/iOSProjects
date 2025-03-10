dec8301e5a42b222d40022bdc4a622262e08656e
Srushti Sarju Engineer <srushti.engineer@simformsolutions.com>
2025-03-10 13:17:05 +0530

    TE-T282: Data Objects for UI

---
+++ iOSLearningProject/iOSLearningProject/UIComponents/Data Objects/Books.swift
@@ -0,0 +1,36 @@
+//
+//  Books.swift
+//  UIOne
+//
+//  Created by Srushti Sarju Engineer on 03/03/25.
+//
+
+struct Book {
+    var name: String
+    var author: String
+}
+
+//let bookCollection: [String : [Book]] = [
+//    "Growth" : [
+//        Book(name: "Cosmos", author: "Carl Sagon"),
+//        Book(name: "Atomic Habits", author: "James Clear"),
+//        Book(name: "The Power of Habit", author: "Charles Duhigg"),
+//        Book(name: "Meditations", author: "Marcus Arelius"),
+//        Book(name: "Flow", author: "Mihaly Csikszentmihalyi")
+//    ],
+//    "Knowledge" : [
+//        Book(name: "Sapiens", author: "Yuval Noah Harari"),
+//        Book(name: "Rich Dad, Poor Dad", author: "Robert kiwosaki"),
+//        Book(name: "Stay Hungry, Stay Foolish", author: "xxx")
+//    ]
+//]
+
+let bookCollection = [
+    Book(name: "Cosmos", author: "Carl Sagon"),
+    Book(name: "Atomic Habits", author: "James Clear"),
+    Book(name: "The Power of Habit", author: "Charles Duhigg"),
+    Book(name: "Meditations", author: "Marcus Arelius"),
+    Book(name: "Flow", author: "Mihaly Csikszentmihalyi"),
+    Book(name: "Sapiens", author: "Yuval Noah Harari"),
+    Book(name: "Rich Dad, Poor Dad", author: "Robert kiwosaki"),
+]
